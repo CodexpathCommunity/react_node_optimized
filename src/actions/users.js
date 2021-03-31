d313dbad22742d9314cc909a61768767663df518
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-03-31 04:59:45 +0100

    worked on redux trunk

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,6 +1,17 @@
+import Axios from "axios";
+
+export const initiateAddUsers = () => {
+  return (dispatch) => {
+    Axios.get("/users").then((response) => {
+      console.log(response.data);
+      dispatch(addUsers(response.data.results));
+    });
+  };
+};
+
 export const addUsers = (users) => {
-    return {
-        type: 'ADD_USERS',
-        users
-    };
+  return {
+    type: "ADD_USERS",
+    users,
+  };
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
-import { createStore } from 'redux';
-import usersReducer from '../reducers/users';
+import { createStore } from "redux";
+import usersReducer from "../reducers/users";
+import thunk from "redux-thunk";
 
-const store = createStore(usersReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(usersReducer, applyMiddleware(thunk));
 
 store.subscribe(() => {
-    console.log('store data:', store.getState());
+  console.log("store data:", store.getState());
 });
 
 export default store;
